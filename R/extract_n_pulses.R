d8dc854bf98247d8edf5aa0b90b316c2a90ecd9d
Thomas Sostarics <tsostarics@gmail.com>
2022-08-05 14:17:19 -0500

    Change extract_equal_pulses to extract_n_pulses

--- R/extract_n_pulses.R
+++ R/extract_n_pulses.R
@@ -15,20 +15,23 @@
 #'
 #' @examples
 #'
-#' extract_equal_pulses(data.frame(file = 'a', hz = 9:18), n_pulses = 3, .keep_indices = TRUE)
-extract_equal_pulses <- function(pitchtier_df, n_pulses = 30, .keep_indices = FALSE) {
+#' extract_n_pulses(data.frame(file = 'a', hz = 9:18), n_pulses = 3, .keep_indices = TRUE)
+extract_n_pulses <- function(pitchtier_df,
+                                 n_pulses = 30,
+                                 .keep_indices = FALSE,
+                                 .grouping = "file") {
   n_pulses <- as.integer(n_pulses)
 
   # Determine how many pulses each file has
   pulse_counts <-
     pitchtier_df |>
-    dplyr::group_by(file) |>
+    dplyr::group_by(across(all_of(.grouping))) |>
     dplyr::summarize(n_samples = dplyr::n())
 
   # Ensure that every file has at least n_pulses pulses
   if (any(pulse_counts[['n_samples']] < n_pulses)) {
     min_index <- which.min(pulse_counts[['n_samples']])
-    min_file <- pulse_counts[['file']][min_index]
+    min_file <- pulse_counts[[.grouping]][min_index]
     min_count <- pulse_counts[['n_samples']][min_index]
     stop(glue::glue("{n_pulses} pulses requested but {min_file} only has {min_count}"))
   }
@@ -38,14 +41,14 @@ extract_equal_pulses <- function(pitchtier_df, n_pulses = 30, .keep_indices = FA
     lapply(pulse_counts[['n_samples']],
            \(x)
            round(seq(1, x, length.out = n_pulses), 0))
-  names(pulse_indices) <- pulse_counts[['file']]
+  names(pulse_indices) <- pulse_counts[[.grouping]]
 
   # Extract the calculated pulses
   pitchtier_df <-
     pitchtier_df |>
-    dplyr::group_by(file) |>
+    dplyr::group_by(across(all_of(.grouping))) |>
     dplyr::mutate(pulse_i = dplyr::row_number(),
-                  keep_pulse = .data$pulse_i %in% pulse_indices[[dplyr::first(file)]]) |>
+                  keep_pulse = .data$pulse_i %in% pulse_indices[[dplyr::first(.data[[.grouping]])]]) |>
     dplyr::filter(.data$keep_pulse) |>
     dplyr::select(-'keep_pulse')
 
